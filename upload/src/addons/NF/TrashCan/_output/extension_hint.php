1ac08536efc96bbb77b96aa51f6128c87dc75ea7
Zach <z61@zeesixtyone.com>
2019-12-27 15:28:18 -0500

    Add _output folder

---
+++ upload/src/addons/NF/TrashCan/_output/extension_hint.php
@@ -0,0 +1,19 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace NF\TrashCan\XF\Entity
+{
+	class XFCP_Thread extends \XF\Entity\Thread {}
+}
+
+namespace NF\TrashCan\XF\InlineMod
+{
+	class XFCP_Thread extends \XF\InlineMod\Thread {}
+}
+
+namespace NF\TrashCan\XF\Pub\Controller
+{
+	class XFCP_Thread extends \XF\Pub\Controller\Thread {}
+}
